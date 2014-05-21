0737cc4dba702ea382038abb0bcf09e9800ba2b8
Ian Yates <ian@snaptin.com>
2014-05-21 17:03:36 +0200

    first commit

---
+++ js/init.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){
+	var smallWindow = false;
+
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+
+		if (scroll >= 50) {
+			$('#logo-image').attr('src', 'img/Acme_Monogram_Colour.png')
+			$(".important-class").addClass("padding-on-my-header");
+		}
+		if (scroll < 50) {
+			$(".important-class").removeClass("padding-on-my-header");
+			$('#logo-image').attr('src', 'img/Acme_Colour.png')
+		}
+	}).resize(function(){
+		if ( !smallWindow && this.innerWidth <= 1024 ) {
+			smallWindow = true;
+			$('.top-bar-section').find('ul.right').hide(0).delay(500).show(0);
+		}
+		if ( smallWindow && this.innerWidth > 1024 ) {
+			smallWindow = false;
+		}
+	});
+});
